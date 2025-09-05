d8832b64b6cc3d7fe1a796731335c8a682b904ca
Tryanks <tryanks@outlook.com>
2025-09-05 16:07:13 +0800

    Update module path and references to new repository ownership

--- examples/main.go
+++ examples/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/electricbubble/gadb"
+	"github.com/Tryanks/gadb"
 	"log"
 	"os"
 	"strings"
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/electricbubble/gadb
+module github.com/Tryanks/gadb
 
-go 1.15
+go 1.25
